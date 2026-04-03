9927523f73747294c7449244f7ef8d338438cfe9
brainiac2030 <isaackiambo@gmail.com>
2026-04-03 22:33:29 +0300

    full code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+const form = document.getElementById('create-task-form');
+
+
+form.addEventListener('submit', (event) => {
+  
+  event.preventDefault();
+
+  
+  const taskDescription = event.target['new-task-description'].value;
+
+  
+  buildToDo(taskDescription);
+
+  
+  event.target['new-task-description'].value = '';
+});
+
+
+function buildToDo(task) {
+  
+  const li = document.createElement('li');
+  
+  
+  li.textContent = task;
+  
+ 
+  const taskList = document.getElementById('tasks');
+  taskList.appendChild(li);
+}
 });
